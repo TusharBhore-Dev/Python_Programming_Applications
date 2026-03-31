073948fae4cc310ef3f397731c6ba59c5a818805
Tushar Bhore <tusharvbhore@gmail.com>
2026-03-31 22:07:36 +0530

    Add files via upload

---
+++ Problems_On_Pattern/DisplayDigitTriangle.py
@@ -0,0 +1,121 @@
+# /////////////////////////////////////////////////////////
+# //////
+# ////                       1
+# ///     Expected Pattern : 1    2   
+# //                         1    2   3
+# ///                        1    2   3   4 
+# ////
+# /////
+# /////////////////////////////////////////////////////////
+
+# /////////////////////////////////////////////////////////////////
+# //
+# //  Function Name : DisplayPattern()
+# //  Description :   It is used to display the above pattern. 
+# //  Input :         int , int
+# //  Output :        pattern
+# //  Author :        Tushar vikas bhore.
+# //  Date :          31/03/2025
+# //
+# /////////////////////////////////////////////////////////////////
+
+def DisplayPattern( iRow , iCol ):
+
+    for i in range( 1 , iRow + 1 ):
+
+        for j in range( 1 , i + 1 ):
+
+            print( j , end ="\t")
+    
+        print()
+
+# End of DisplayPattern() 
+
+#################################################################
+#####
+###
+#       Entry Point Function Of Application
+###
+#####
+#################################################################
+
+def main():
+
+    iRowLim = 0
+    iColLim = 0
+
+    print( "Enter how many rows you want to display :\t" )
+    iRowLim = int( input() )
+
+    print( "Enter how many coloms you want to display :\t" )
+    iColLim = int( input() )
+
+    if iRowLim != iColLim :
+
+        print( "Error : Please provide the valid input.( Equal  and positive number of rows and coloms )" )
+        
+        return
+
+    DisplayPattern( iRowLim , iColLim )
+
+#    // End of main
+
+################################################
+####
+###
+#       Starter Function For The APPLICATION
+###
+####
+################################################
+
+if __name__ == "__main__":
+    
+    main()
+
+#End of starter funtion
+
+###########################################################
+###########################################################
+################################
+#
+#       TestCases Successfully Handled By The Application
+#
+########
+##########  TestCase 1 :
+########
+#               Enter how many rows you want to display :
+#                   4
+#               Enter how many coloms you want to display :
+#                   4
+#                   1
+#                   1       2
+#                   1       2       3
+#                   1       2       3       4
+#
+#
+########
+##########  TestCase 2 :
+########
+#
+#               Enter how many rows you want to display :
+#                   5
+#               Enter how many coloms you want to display :
+#                   5
+#                   1
+#                   1       2
+#                   1       2       3
+#                   1       2       3       4
+#                   1       2       3       4       5   
+########
+##########  TestCase 3 :
+########
+#
+#                Enter how many rows you want to display :
+#                   4
+#                Enter how many coloms you want to display :
+#                   5
+#                   Error : Please provide the valid input.( Equal  and positive number of rows and coloms )
+#
+#################################
+##########################################################
+##########################################################
---
+++ Problems_On_Pattern/DisplaySquareWithDigonalWhile.py
@@ -0,0 +1,139 @@
+# /////////////////////////////////////////////////////////
+# //////
+# ////                       * * * * *
+# ///     Expected Pattern : * *     *
+# //                         *   *   *
+# ///                        *     * *
+# ////                       * * * * *
+# /////
+# /////////////////////////////////////////////////////////
+
+
+# /////////////////////////////////////////////////////////////////
+# //
+# //  Function Name : DisplayPattern()
+# //  Description :   It is used to display the above pattern. 
+# //  Input :         int , int
+# //  Output :        pattern
+# //  Author :        Tushar vikas bhore.
+# //  Date :          31/03/2025
+# //
+# /////////////////////////////////////////////////////////////////
+
+def DisplayPattern( iRow , iCol ):
+
+    i = 1
+    j = 1
+
+    while( i < iRow + 1 ):
+
+        j = 1
+        
+        while( j < iCol + 1 ):
+
+
+            if( i == 1 or j == 1 or i == iRow or j == iCol or i == j  ):
+
+                print( "*" , end = "\t" )
+            
+            else:
+
+                print( " " , end = "\t" )
+                
+            j = j + 1
+        
+        print( "\n" )
+        i = i + 1
+
+
+# End of DisplayPattern() 
+
+
+#################################################################
+#####
+###
+#       Entry Point Function Of Application
+###
+#####
+#################################################################
+
+def main():
+
+    iRowLim = 0
+    iColLim = 0
+
+    print( "Enter how many rows you want to display :\t" )
+    iRowLim = int( input() )
+
+    print( "Enter how many coloms you want to display :\t" )
+    iColLim = int( input() )
+
+    if iRowLim != iColLim :
+
+        print( "Error : Please provide the valid input.( Equal  and positive number of rows and coloms )" )
+        
+        return
+
+    DisplayPattern( iRowLim , iColLim )
+
+#    // End of main
+
+################################################
+####
+###
+#       Starter Function For The APPLICATION
+###
+####
+################################################
+
+if __name__ == "__main__":
+    
+    main()
+
+#End of starter funtion
+
+###########################################################
+###########################################################
+################################
+#
+#       TestCases Successfully Handled By The Application
+#
+########
+##########  TestCase 1 :
+########
+#               
+#               Enter how many rows you want to display :
+#                   5
+#               Enter how many coloms you want to display :
+#                   5
+#                   *       *       *       *       *
+#
+#                   *       *                       *
+#
+#                   *               *               *
+#
+#                   *                       *       *
+#
+#                   *       *       *       *       *
+#
+#
+########
+##########  TestCase 2 :
+########
+# 
+#               Enter how many rows you want to display :
+#                   4
+#               Enter how many coloms you want to display :
+#                   4
+#                   *       *       *       *
+#
+#                   *       *               *
+#
+#                   *               *       *
+#
+#                   *       *       *       *
+#
+#
+#################################
+##########################################################
+##########################################################
